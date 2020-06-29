37bfdd15fbb2066c1d559b101398336cabb5a6cd
Alex Camargo <alexcamargo13@gmail.com>
2020-06-29 14:47:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function yourAnimal() {
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: Hard-coanimal ='cat'
-  return animal
+  return 'cat'
 }
 
 function add2(n) {
